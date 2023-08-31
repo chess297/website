62ec512031826e4648b4ffcd9fe5a9750d55274c
chenyunqi <viky.chen@outlook.com>
2023-09-01 04:39:32 +0800

    修改配置

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Viky'Site",
   tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  // favicon: "img/favicon.ico",
 
   // Set the production url of your site here
   url: "https://chen-viky.github.io",
