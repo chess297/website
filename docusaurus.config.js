95172584b6870a6d9840e95e5edd6e21bbe43926
chenyunqi <viky.chen@outlook.com>
2023-09-01 04:16:12 +0800

    reset blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "chen-viky", // Usually your GitHub org/user name.
-  projectName: "chen-viky-blog", // Usually your repo name.
+  projectName: "blog", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
