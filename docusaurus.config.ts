9040f588b11b45bdc4a3e497a6db8fef4c723927
chenyunqi <viky.chen@outlook.com>
2025-03-19 22:22:35 +0800

    update name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,19 +5,19 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "Viky‘ Site",
+  title: "Chess‘ Site",
   tagline: "More Professional",
   // favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://viky-chen.github.io",
+  url: "https://chess297.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/website/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "viky-chen", // Usually your GitHub org/user name.
+  organizationName: "chess297", // Usually your GitHub org/user name.
   projectName: "website", // Usually your repo name.
 
   onBrokenLinks: "throw",
@@ -39,7 +39,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: "https://github.com/viky-chen/website",
+          // editUrl: "https://github.com/Chess-chen/website",
         },
 
         theme: {
@@ -53,9 +53,9 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "Viky' Site",
+      title: "Chess' Site",
       logo: {
-        alt: "Viky' Site",
+        alt: "Chess' Site",
         src: "img/avatar.jpeg",
         style: {
           borderRadius: "50%",
@@ -64,27 +64,9 @@ const config: Config = {
       items: [
         {
           type: "docSidebar",
-          sidebarId: "languageSidebar",
-          position: "left",
-          label: "Language",
-        },
-        {
-          type: "docSidebar",
           sidebarId: "webSidebar",
           position: "left",
           label: "Web",
-        },
-        {
-          type: "docSidebar",
-          sidebarId: "mobileSidebar",
-          position: "left",
-          label: "Mobile",
-        },
-        {
-          type: "docSidebar",
-          sidebarId: "desktopSidebar",
-          position: "left",
-          label: "Desktop",
         },
         {
           type: "docSidebar",
@@ -99,19 +81,7 @@ const config: Config = {
           label: "AI",
         },
         {
-          type: "docSidebar",
-          sidebarId: "mathSidebar",
-          position: "left",
-          label: "Math",
-        },
-        {
-          type: "docSidebar",
-          sidebarId: "englishSidebar",
-          position: "left",
-          label: "English",
-        },
-        {
-          href: "https://github.com/viky-chen",
+          href: "https://github.com/chess297",
           label: "GitHub",
           position: "right",
         },
